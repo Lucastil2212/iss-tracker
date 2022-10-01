3e411d6219441e8748a60cf6f18f67ad1100629e
Lucas Tilford <lmt95@case.edu>
2022-10-01 14:08:06 -0400

    site init

---
+++ ui/src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import Globe from "worldwind-react-globe";
+
+export default function home() {
+  return (
+    <div>
+      <h1>ISS Tracker</h1>
+      <Globe />
+    </div>
+  );
+}
